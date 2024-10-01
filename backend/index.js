a985536c5340e328b2c229b4ccda7eb90fbae2f5
prajapatsachin7078 <sp639856@gmail.com>
2024-10-01 10:02:12 +0530

    removed corsoptoin to test deployment

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,11 @@ app.use(json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "http://localhost:5173",
+//     credentials: true
+// };
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
