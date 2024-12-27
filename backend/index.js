4faca3927d56c85e8c821b5a0b3160fd933324d6
prajapatsachin7078 <sp639856@gmail.com>
2024-12-27 14:21:33 +0530

    cors-options-updated

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true
 };
 app.use(cors(corsOptions));
